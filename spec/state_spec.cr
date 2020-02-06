c6e51a5de48fb42b3ab8642cffea35018c56cba4
Juan Wajnerman <waj@manas.tech>
2020-02-06 17:42:37 -0300

    Fixed DependencyState specs

--- spec/state_spec.cr
+++ spec/state_spec.cr
@@ -4,23 +4,25 @@ module Molinillo
   describe ResolutionState do
     describe DependencyState do
       it "pops a possibility state" do
-        state = DependencyState(Nil, String, String).new(
+        possibility1 = Resolver::Resolution::PossibilitySet(String, String).new(%w(), %w(possibility1))
+        possibility = Resolver::Resolution::PossibilitySet(String, String).new(%w(), %w(possibility))
+        state = DependencyState(String, String).new(
           "name",
           %w(requirement1 requirement2 requirement3),
-          DependencyGraph(Nil, String).new,
+          DependencyGraph(Resolver::Resolution::PossibilitySet(String, String) | String | Nil, String).new,
           "requirement",
-          %w(possibility1 possibility),
+          [possibility1, possibility],
           0,
-          {} of String => Nil,
-          [] of Nil
+          {} of String => Resolver::Resolution::Conflict(String, String),
+          [] of Resolver::Resolution::UnwindDetails(String, String)
         )
         possibility_state = state.pop_possibility_state
-        # %w(name requirements activated requirement conflicts).each do |attr|
-        #   expect(possibility_state.send(attr)).to eq(state.send(attr))
-        # end
-        possibility_state.should be_a(PossibilityState(Nil, String, String))
+        {% for attr in %w(name requirements activated requirement conflicts) %}
+          possibility_state.{{ attr.id }}.should eq(state.{{ attr.id }})
+        {% end %}
+        possibility_state.should be_a(PossibilityState(String, String))
         possibility_state.depth.should eq(state.depth + 1)
-        possibility_state.possibilities.should eq(%w(possibility))
+        possibility_state.possibilities.should eq([possibility])
       end
     end
   end
