e461e462dc06a9ac9266e9ea6e3eb395a3802855
Sijawusz Pur Rahnama <sija@sija.pl>
2022-03-07 19:11:13 +0100

    Refactor code according to @beta-ziliani suggestion

--- spec/spec_helper/specification.cr
+++ spec/spec_helper/specification.cr
@@ -19,20 +19,20 @@ module Molinillo
 
   module DepConverter
     def self.from_json(parser)
-      deps =
-        if parser.kind.begin_object?
-          Hash(String, String).new(parser)
-        else
-          Hash(String, String).new.tap do |hash|
-            parser.read_array do
-              parser.read_begin_array
-              key = parser.read_string
-              value = parser.read_string
-              parser.read_end_array
-              hash[key] = value
-            end
-          end
+      if parser.kind.begin_object?
+        deps = Hash(String, String).new(parser)
+      else
+        deps = Hash(String, String).new
+
+        parser.read_array do
+          parser.read_begin_array
+          key = parser.read_string
+          value = parser.read_string
+          parser.read_end_array
+
+          deps[key] = value
         end
+      end
 
       deps.map do |name, requirement|
         requirements = requirement.split(',').map!(&.chomp)
